5c68a0eb8a5dacb1ee10a541579126c7fd5d7921
Karthik <karthikkanna9866@gmail.com>
2023-03-09 18:37:57 +0000

    boxes reactJS

---
+++ index.js
@@ -0,0 +1,23 @@
+const Box = (props) => {
+  //  Write your code here.
+  const { className, name } = props;
+  const boxElement = (
+    <div className={className}>
+      <h1 className="description"> {name} </h1>
+    </div>
+  );
+};
+
+const element = (
+  //  Write your code here.
+  <div className="bg-container">
+    <h1 className="heading">Boxes</h1>
+    <div className="boxesContainer">
+      <Box className="smallBox" name="Small" />
+      <Box className="mediumBox" name="Medium" />
+      <Box className="largeBox" name="Large" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
